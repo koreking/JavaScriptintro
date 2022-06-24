8890eab90c1a16d07e1b4800e37a6b1f00298f1a
Martins <105179117+koreking@users.noreply.github.com>
2022-06-24 14:12:20 +0100

    Added introduction to javascript files

---
+++ display.js
@@ -0,0 +1,15 @@
+let Name = "Martins Ogundele"
+
+let Country = "Nigeria"
+
+let height = "5'7"
+
+
+document.write("My name is &nbsp", Name);
+
+
+document.write("<br> I am &nbsp", height, "&nbsp tall")
+
+
+document.write( "<br> My country is &nbsp", Country)
+
